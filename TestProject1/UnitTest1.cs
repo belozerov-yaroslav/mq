21ab2d8db75db1b5f30de6219341220bf9f6701e
belozerov-yaroslav <jarblz@yandex.ru>
2025-11-25 16:24:30 +0500

    test

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,66 +1,31 @@
-﻿using MassTransit;
-using MassTransit.Testing;
-using Microsoft.Extensions.DependencyInjection;
-using NUnit.Framework;
-
-using consumer;
+﻿using MassTransit.Testing;
 using Models;
 
-namespace ProducerConsumer.Tests;
+namespace consumer;
 
-public class RabbitMqTests
+public class MyConsumerTest
 {
-    private ServiceProvider _provider = null!;
-    private ITestHarness _harness = null!;
-
-    private IServiceScope _scope = null!;
-
-    [SetUp]
-    public async Task Setup()
-    {
-        _provider = new ServiceCollection()
-            .AddMassTransitTestHarness(cfg =>
-            {
-                cfg.AddConsumer<MyConsumer>();
-            })
-            .AddScoped<MyPublisher>()
-            .BuildServiceProvider(true);
-
-        _harness = _provider.GetRequiredService<ITestHarness>();
-        await _harness.Start();
-
-        // Создаем scoped-сервисы для теста
-        _scope = _provider.CreateScope();
-    }
-
-    [TearDown]
-    public async Task TearDown()
-    {
-        await _harness.Stop();
-        await _provider.DisposeAsync();
-        _scope.Dispose();
-    }
-
     [Test]
-    public async Task Publisher_Should_Send_And_Consumer_Should_Receive()
+    public async Task MsgConsumeTest()
     {
-        // получаем scoped MyPublisher
-        var publisher = _scope.ServiceProvider.GetRequiredService<MyPublisher>();
-        var text = "hello test";
+        var harness = new InMemoryTestHarness();
+        var consumerHarness = harness.Consumer<MyConsumer>();
 
-        await publisher.Publish(text);
+        await harness.Start();
 
-        // сообщение отправлено
-        Assert.That(await _harness.Sent.Any<Class1>(), Is.True);
-
-        // сообщение получено consumer
-        Assert.That(await _harness.Consumed.Any<Class1>(), Is.True);
-
-        // дополнительно проверяем payload
-        var consumed = _harness.Consumed
-            .Select<Class1>()
-            .First().Context.Message;
-
-        Assert.That(consumed.message, Is.EqualTo(text));
+        await harness.InputQueueSendEndpoint.Send(new Class1 { message = "test msg"});
+        await harness.InputQueueSendEndpoint.Send(new Class1 { message = "test msg2"});
+        
+        var message = harness.Consumed.Select<Class1>().First().MessageObject as Class1;
+        var consumedMessage = consumerHarness.Consumed.Select<Class1>().FirstOrDefault().MessageObject as Class1;
+        
+        Assert.That(message!.message, Is.EqualTo("test msg"));
+        Assert.That(consumedMessage!.message, Is.EqualTo("test msg"));
+        
+        message = harness.Consumed.Select<Class1>().Skip(1).First().MessageObject as Class1;
+        consumedMessage = consumerHarness.Consumed.Select<Class1>().Skip(1).First().MessageObject as Class1;
+        
+        Assert.That(message!.message, Is.EqualTo("test msg2"));
+        Assert.That(consumedMessage!.message, Is.EqualTo("test msg2"));
     }
 }
